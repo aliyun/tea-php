8400dde9529d8e91e6ceb1890b89ed2dc1eae25c
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-04-13 15:10:20 +0800

    TeaUnableRetryError add $lastException param

--- src/Exception/TeaUnableRetryError.php
+++ src/Exception/TeaUnableRetryError.php
@@ -6,27 +6,33 @@ use AlibabaCloud\Tea\Request;
 use RuntimeException;
 
 /**
- * Class TeaUnableRetryError
- *
- * @package AlibabaCloud\Tea\Exception
+ * Class TeaUnableRetryError.
  */
 class TeaUnableRetryError extends RuntimeException
 {
     private $lastRequest;
+    private $lastException;
 
     /**
      * TeaUnableRetryError constructor.
      *
-     * @param Request         $lastRequest
+     * @param Request    $lastRequest
+     * @param \Exception $lastException
      */
-    public function __construct($lastRequest)
+    public function __construct($lastRequest, $lastException = null)
     {
         parent::__construct('TeaUnableRetryError', 0, null);
-        $this->lastRequest = $lastRequest;
+        $this->lastRequest   = $lastRequest;
+        $this->lastException = lastException;
     }
 
     public function getLastRequest()
     {
         return $this->lastRequest;
+    }
+
+    public function getLastException()
+    {
+        return  $this->lastException;
     }
 }
